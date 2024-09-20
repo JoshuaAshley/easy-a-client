3cdaa2b0ae68d832fd8603b5e23da7879f39d616
Gerard <104638929+st10245621@users.noreply.github.com>
2024-09-20 14:45:49 +0200

    updated login page

--- app/src/main/java/easy_a/controllers/SplashActivity.kt
+++ app/src/main/java/easy_a/controllers/SplashActivity.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import com.example.easy_a.R
-import com.example.easy_a.controllers.MainScreen
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)  // Link to your XML layout
+        setContentView(R.layout.activity_splash)  // Link to your splash screen XML layout
 
         // Simulate a delay for the splash screen (3 seconds)
         Handler().postDelayed({
-            // Move to the MainActivity after the delay
-            val intent = Intent(this, MainScreen::class.java)
+            // Move to the LoginScreen after the delay
+            val intent = Intent(this, LoginScreen::class.java)
             startActivity(intent)
             finish()  // Close the SplashActivity so it is not accessible again
         }, 3000)  // 3000 milliseconds = 3 seconds
