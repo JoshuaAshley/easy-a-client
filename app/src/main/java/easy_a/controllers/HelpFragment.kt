d2602042d6cb411847005a0ec2dfaa2eebaa63f7
Dane <92745810+INDWG@users.noreply.github.com>
2024-09-23 16:15:18 +0200

    Study Page

---
+++ app/src/main/java/easy_a/controllers/HelpFragment.kt
@@ -0,0 +1,19 @@
+package easy_a.controllers
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.easy_a.R
+
+class HelpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.help_fragment, container, false)
+    }
+}
